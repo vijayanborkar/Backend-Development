b410be066deff06f45420dc376c8bf5edf65c784
Vijayan Borkar <90171659+vijayanborkar@users.noreply.github.com>
2024-08-21 04:18:19 +0000

    BD4.1_CW

--- BD4.1_CW/index.js
+++ BD4.1_CW/index.js
@@ -8,41 +8,71 @@ let db;
 
 // Connect to SQLite database
 (async () => {
-  try {
-    db = await open({ filename: "database.sqlite", driver: sqlite3.Database });
-    console.log("Connected to the SQLite database.");
-
-    // Start the server only after the database connection is established
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to connect to the database:", error.message);
-    process.exit(1); // Exit the application if the database connection fails
-  }
+  db = await open({ filename: "database.sqlite", driver: sqlite3.Database });
+  if (db) console.log("Connected to the SQLite database.");
 })();
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "BD4.1 CW - SQL Queries & async/await" });
 });
 
+// YOUR ENPOINTS GO HERE
+
 // Exercise 1: Fetch all movies
 async function fetchAllMovies() {
-  try {
-    let query = "SELECT * FROM movies";
-    let response = await db.all(query);
-    return { movies: response };
-  } catch (error) {
-    console.error("Failed to fetch movies:", error.message);
-    throw error;
-  }
+  let query = "SELECT * FROM movies";
+  let response = await db.all(query, []);
+
+  return { movies: response };
 }
 
 app.get("/movies", async (req, res) => {
-  try {
-    let results = await fetchAllMovies();
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch movies" });
-  }
+  let results = await fetchAllMovies();
+
+  res.status(200).json(results);
+});
+
+// Exercise 2: Fetch all movies by genre
+async function fetchMovieByGenre(genre) {
+  let query = "SELECT * FROM movies WHERE genre = ?";
+  let response = await db.all(query, [genre]);
+
+  return { movies: response };
+}
+
+app.get("/movies/genre/:genre", async (req, res) => {
+  let genre = req.params.genre;
+  let result = await fetchMovieByGenre(genre);
+
+  res.status(200).json(result);
+});
+
+// Exercise 3: Fetch movie details by ID
+async function fetchMovieDetailsById(id) {
+  let query = "SELECT * FROM movies WHERE id = ?";
+  let response = await db.all(query, [id]);
+  return { id: response };
+}
+
+app.get("/movies/details/:id", async (req, res) => {
+  let id = req.params.id;
+  let result = await fetchMovieDetailsById(id);
+  res.status(200).json(result);
+});
+
+// Exercise 4: Fetch movie details by release_year
+async function fetchMovieDetailsByReleaseYear(year) {
+  let query = "SELECT * FROM movies WHERE release_year = ?";
+  let response = await db.all(query, [year]);
+  return { year: response };
+}
+
+app.get("/movies/release_year/:year", async (req, res) => {
+  let year = req.params.year;
+  let result = await fetchMovieDetailsByReleaseYear(year);
+  res.status(200).json(result);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
